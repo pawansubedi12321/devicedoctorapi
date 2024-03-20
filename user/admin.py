443e1fb47d7cbd43cda3a0a9babe4c087341e9c6
pawan <pawansubedi39@gmail.com>
2024-03-20 08:09:52 +0545

    adduser

--- user/admin.py
+++ user/admin.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import AddCategory,Problem,Question,Answer,createBooking,Register
-# Register your models here.
 
+class createBookingInline(admin.TabularInline):
+    model = createBooking
+    extra = 1  # Number of extra forms to show
 
+class createprobleminline(admin.ModelAdmin):
+    inlines = [createBookingInline]
+       
 admin.site.register(AddCategory)
-admin.site.register(Problem)
+# admin.site.register(Problem)
 admin.site.register(Question)
 admin.site.register(Answer)
-admin.site.register(createBooking)
+admin.site.register(Problem,createprobleminline)
 admin.site.register(Register)
